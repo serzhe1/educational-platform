456f1d3696846fc2016986905af6651cc2a72edc
s.ladygin <s.ladygin@click-group.ru>
2023-04-29 13:23:23 +0300

    добавил сервер авторизации

--- course-manager-service/src/main/java/com/edpl/cms/web/controller/CourseController.java
+++ course-manager-service/src/main/java/com/edpl/cms/web/controller/CourseController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/courses")
 @RequiredArgsConstructor
 public class CourseController {
     private final CourseService service;
