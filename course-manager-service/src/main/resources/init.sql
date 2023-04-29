e32048a2e722526437ec9e96630c4def2d15b59c
s.ladygin <s.ladygin@click-group.ru>
2023-04-29 14:17:27 +0300

    init.sql - актуальная бд

--- course-manager-service/src/main/resources/init.sql
+++ course-manager-service/src/main/resources/init.sql
@@ -1,88 +1,98 @@
-DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS users;
-
-
-CREATE TABLE users
-(
-    id       SERIAL PRIMARY KEY,
-    name     VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email    VARCHAR(255) NOT NULL UNIQUE
-);
-
-CREATE TABLE roles
-(
-    id   SERIAL PRIMARY KEY,
-    role VARCHAR(255) NOT NULL UNIQUE
-);
-
-CREATE TABLE user_roles
-(
-    user_id INTEGER REFERENCES users (id),
-    role_id INTEGER REFERENCES roles (id),
-    PRIMARY KEY (user_id, role_id)
-);
-
-INSERT INTO roles (role)
-VALUES ('ADMIN'),
-       ('TEACHER'),
-       ('OWNER'),
-       ('STUDENT');
--- -----------------------------------------------------------------------------------------------
+drop table if exists courses;
 create table courses
 (
-    id          serial primary key,
-    "name"      varchar,
-    description varchar
+    id           serial primary key,
+    description  varchar,
+    format       varchar,
+    requirements varchar,
+    competencies varchar
 );
 
-create table user_course
-(
-    id        serial primary key,
-    user_id   integer,
-    course_id integer,
-
-    constraint user_fk foreign key (user_id) references users (id),
-    constraint course_fk foreign key (course_id) references courses (id)
-
-);
-
-create table theory_module
+drop table if exists modules;
+create table modules
 (
     id          serial primary key,
-    "name"      varchar,
+    name        varchar,
     description varchar,
-    course_id   integer,
-
-    constraint course_fk foreign key (course_id) references courses (id)
+    course_id   bigint,
+    constraint course_fk
+        foreign key (course_id)
+            references courses (id)
 );
 
-create table tasks
-(
-    id               serial primary key,
-    description      varchar,
-    theory_module_id integer,
-
-    constraint theory_module_fk foreign key (theory_module_id) references theory_module (id)
-);
-
-create table answers
+drop table if exists lectures;
+create table lectures
 (
     id        serial primary key,
-    answer    varchar,
-    isCorrect bool,
-    task_id   integer,
-
-    constraint task_fk foreign key (task_id) references tasks (id)
+    content   varchar,
+    module_id bigint,
+    constraint module_fk
+        foreign key (module_id)
+            references modules (id)
 );
 
-create table theory_part
+drop table if exists tests;
+create table tests
 (
-    id               serial primary key,
-    title            varchar,
-    "content"          varchar,
-    theory_module_id integer,
+    id        serial primary key,
+    question  varchar,
+    module_id bigint,
+    constraint module_fk
+        foreign key (module_id)
+            references modules (id)
+);
 
-    constraint theory_module_fk foreign key (theory_module_id) references theory_module
+drop table if exists test_answers;
+create table test_answers
+(
+    id       serial primary key,
+    answer   varchar,
+    is_right boolean,
+    test_id  bigint,
+    constraint test_id
+        foreign key (test_id)
+            references tests (id)
+);
+
+drop table if exists users;
+create table users
+(
+    id        serial primary key,
+    username  varchar,
+    full_name varchar
+);
+
+drop table if exists roles;
+create table roles
+(
+    id   serial primary key,
+    role varchar
+);
+
+drop table if exists user_roles;
+create table user_roles
+(
+    user_id bigint,
+    role_id bigint,
+    primary key (user_id, role_id),
+    constraint user_fk
+        foreign key (user_id)
+            references users (id),
+    constraint role_fk
+        foreign key (role_id)
+            references roles (id)
+);
+
+drop table if exists user_courses;
+create table user_courses
+(
+    user_id bigint,
+    course_id bigint,
+    primary key (user_id, course_id),
+    constraint user_fk
+        foreign key (user_id)
+            references users (id),
+    constraint course_fk
+        foreign key (course_id)
+            references courses (id)
 )
