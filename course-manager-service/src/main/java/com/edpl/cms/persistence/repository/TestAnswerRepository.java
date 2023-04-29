bb7a4991a023033be52d52dc1e08d4d70958a74c
Artur <oxygen12ium3@gmail.com>
2023-04-29 18:24:14 +0300

    убрал черкаши

--- course-manager-service/src/main/java/com/edpl/cms/persistence/repository/TestAnswerRepository.java
+++ course-manager-service/src/main/java/com/edpl/cms/persistence/repository/TestAnswerRepository.java
@@ -1,9 +1,9 @@
 package com.edpl.cms.persistence.repository;
 
 import com.edpl.cms.persistence.model.TestAnswersEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestAnswerRepository extends CrudRepository<TestAnswersEntity, Long> {
+public interface TestAnswerRepository extends JpaRepository<TestAnswersEntity, Long> {
 }
--- course-manager-service/src/main/java/com/edpl/cms/persistence/repository/TestRepository.java
+++ course-manager-service/src/main/java/com/edpl/cms/persistence/repository/TestRepository.java
@@ -1,9 +1,9 @@
 package com.edpl.cms.persistence.repository;
 
 import com.edpl.cms.persistence.model.TestEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestRepository extends CrudRepository<TestEntity, Long> {
+public interface TestRepository extends JpaRepository<TestEntity, Long> {
 }
