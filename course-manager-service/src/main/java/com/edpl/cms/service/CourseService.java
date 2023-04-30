9e2a3b26d61cb3b561eacf72432daa85b41c99b1
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-04-30 21:23:51 +0300

    создал метод update в CourseService

--- course-manager-service/src/main/java/com/edpl/cms/service/CourseService.java
+++ course-manager-service/src/main/java/com/edpl/cms/service/CourseService.java
@@ -15,35 +15,51 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CourseService {
-    private final ModelMapper modelMapper;
+	private final ModelMapper modelMapper;
 
-    private final CourseRepository courseRepository;
+	private final CourseRepository courseRepository;
 
-    @Transactional(readOnly = true)
-    public List<CourseDto> findAll() {
-        return courseRepository
-                .findAll().stream()
-                .map(courseEntity -> modelMapper.map(courseEntity, CourseDto.class))
-                .toList();
-    }
+	@Transactional(readOnly = true)
+	public List<CourseDto> findAll() {
+		return courseRepository
+			.findAll().stream()
+			.map(courseEntity -> modelMapper.map(courseEntity, CourseDto.class))
+			.toList();
+	}
 
-    @Transactional(readOnly = true)
-    public CourseDto findById(Long id) {
-        CourseEntity ce = courseRepository
-                .findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("Course with {id=%d} not found.", id)));
+	@Transactional(readOnly = true)
+	public CourseDto findById(Long id) {
+		CourseEntity ce = courseRepository
+			.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException(String.format("Course with {id=%d} not found.", id)));
 
-        return modelMapper.map(ce, CourseDto.class);
-    }
+		return modelMapper.map(ce, CourseDto.class);
+	}
 
-    @Transactional
-    public CourseDto save(CourseDto courseDto) {
-        CourseEntity toSave = modelMapper.map(courseDto, CourseEntity.class);
+	@Transactional
+	public CourseDto save(CourseDto courseDto) {
+		CourseEntity toSave = modelMapper.map(courseDto, CourseEntity.class);
 
-        CourseEntity course = courseRepository.save(toSave);
+		CourseEntity course = courseRepository.save(toSave);
 
-        courseDto.setId(course.getId());
+		courseDto.setId(course.getId());
 
-        return courseDto;
-    }
+		return courseDto;
+	}
+
+	@Transactional
+	public CourseDto update(CourseDto courseDto) {
+		CourseEntity course = courseRepository.findById(courseDto.getId())
+											  .orElseThrow(() -> new ResourceNotFoundException(
+												  String.format("Course with id=%d not found", courseDto.getId())));
+
+		course.setCompetencies(courseDto.getCompetencies());
+		course.setFormat(courseDto.getFormat());
+		course.setDescription(courseDto.getDescription());
+		course.setRequirements(course.getRequirements());
+
+		courseRepository.save(course);
+
+		return courseDto;
+	}
 }
