bcb1684ba2c974904feb534a60131f7fd7d5421b
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-04-30 14:15:56 +0300

    глобальная обработка ошибки PersistentObjectException

--- course-manager-service/src/main/java/com/edpl/cms/web/exhandler/GlobalExceptionHandler.java
+++ course-manager-service/src/main/java/com/edpl/cms/web/exhandler/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.edpl.cms.web.exhandler;
 
 import com.edpl.cms.web.exhandler.exceptions.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.PersistentObjectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -24,6 +26,11 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
-
-
+    @ExceptionHandler(PersistentObjectException.class)
+    public ResponseEntity<AppError> catchPersistenceObjectException(PersistentObjectException e) {
+        String customMessage = "you cannot persist entities with non null identifier";
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .body(new AppError(HttpStatus.BAD_REQUEST.value(), customMessage));
+    }
 }
