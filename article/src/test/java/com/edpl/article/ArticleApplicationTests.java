a5387f60fed2acb2502fc5907af23a98debcebec
ogbozoyan <oganes.bozoyan@mail.ru>
2023-05-06 20:18:19 +0300

    init commit

---
+++ article/src/test/java/com/edpl/article/ArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edpl.article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
